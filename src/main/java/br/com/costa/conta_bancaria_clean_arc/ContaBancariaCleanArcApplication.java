f7fb9fe9799bc7ddc91c145018b246f1f11db7f0
Alisson G Costa <alisson.costa84200@gmail.com>
2026-06-15 22:38:39 -0300

    Initial commit

---
+++ src/main/java/br/com/costa/conta_bancaria_clean_arc/ContaBancariaCleanArcApplication.java
@@ -0,0 +1,13 @@
+package br.com.costa.conta_bancaria_clean_arc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContaBancariaCleanArcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContaBancariaCleanArcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/costa/conta_bancaria_clean_arc/ContaBancariaCleanArcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.costa.conta_bancaria_clean_arc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContaBancariaCleanArcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
