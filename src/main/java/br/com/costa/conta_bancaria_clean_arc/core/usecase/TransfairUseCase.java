a8a561901d8b36e2480ad39cf2062ca9fe137cf5
Alisson G Costa <alisson.costa84200@gmail.com>
2026-06-18 22:22:32 -0300

    feat: expose account transfer endpoint

--- src/main/java/br/com/costa/conta_bancaria_clean_arc/core/usecase/TransfairUseCase.java
+++ src/main/java/br/com/costa/conta_bancaria_clean_arc/core/usecase/TransfairUseCase.java
@@ -1,4 +1,29 @@
 package br.com.costa.conta_bancaria_clean_arc.core.usecase;
 
+import br.com.costa.conta_bancaria_clean_arc.core.domain.entitys.AcountEntity;
+import br.com.costa.conta_bancaria_clean_arc.core.gateway.CreateAcountInterface;
+import br.com.costa.conta_bancaria_clean_arc.core.gateway.TransfairInterface;
+
 public class TransfairUseCase {
+
+    private final TransfairInterface transfairInterface;
+
+    public TransfairUseCase(TransfairInterface transfairInterface) {
+        this.transfairInterface = transfairInterface;
+    }
+
+    public void TransfairUseCase(String txNumberToAcount, String txNumberFromAcount) {
+
+        var toacount = transfairInterface.findByTaxNumber(txNumberToAcount)
+               .orElseThrow();
+        var fromacount = transfairInterface.findByTaxNumber(txNumberFromAcount)
+               .orElseThrow();
+
+        var toEntity =  transfairInterface.toEntity(toacount);
+        var fromEntity =  transfairInterface.toEntity(fromacount);
+
+
+
+        transfairInterface.transfairAmount(toAcount, fromAcount);
+    }
 }
--- src/main/java/br/com/costa/conta_bancaria_clean_arc/infrastructure/controller/AcountController.java
+++ src/main/java/br/com/costa/conta_bancaria_clean_arc/infrastructure/controller/AcountController.java
@@ -1,9 +1,12 @@
 package br.com.costa.conta_bancaria_clean_arc.infrastructure.controller;
 
 import br.com.costa.conta_bancaria_clean_arc.core.usecase.CreateAcount;
+import br.com.costa.conta_bancaria_clean_arc.core.usecase.TransfairUseCase;
 import br.com.costa.conta_bancaria_clean_arc.infrastructure.dataprovider.CreateAcountImp;
+import br.com.costa.conta_bancaria_clean_arc.infrastructure.persistence.entity.dto.Response.AcountAmountResponse;
 import br.com.costa.conta_bancaria_clean_arc.infrastructure.persistence.entity.dto.Response.AcountResponse;
 import br.com.costa.conta_bancaria_clean_arc.infrastructure.persistence.entity.dto.request.AcountRequest;
+import br.com.costa.conta_bancaria_clean_arc.infrastructure.persistence.entity.dto.request.AcountRequestAmount;
 import br.com.costa.conta_bancaria_clean_arc.infrastructure.persistence.entity.mapper.AcountMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,6 +19,7 @@ public class AcountController {
 
     private final AcountMapper acountMapper;
     private final CreateAcount  createAcount;
+    private final TransfairUseCase transfairUseCase;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -24,5 +28,14 @@ public class AcountController {
         createAcount.createAcount(acountMapper.toEntity(accountRequest));
         return acountMapper.toResponseEntity(accountRequest);
 
+    }
+
+    @PatchMapping("/{txNumberToAcount}/{txFromAcount}/transfer")
+    @ResponseStatus(HttpStatus.OK)
+    AcountAmountResponse transferAccount(@PathVariable String txNumberToAcount, @PathVariable String txNumberFromAcount, @RequestBody AcountRequestAmount accountRequestAmount ) {
+
+        transfairUseCase.TransfairUseCase(txNumberToAcount, txNumberFromAcount);
+
+        return acountMapper.toResponseAmountEntity(accountRequestAmount);
     }
 }
